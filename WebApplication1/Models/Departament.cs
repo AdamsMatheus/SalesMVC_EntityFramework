4119f2818fc15d8523ce4115c4d0805812bbc89e
Matheus Adams <adamsmatheus7@gmail.com>
2020-04-22 15:21:35 -0300

    Criado

---
+++ WebApplication1/Models/Departament.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebApplication1.Models
+{
+    public class Departament
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Departament()
+        {
+
+        }
+
+        public Departament(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public void AddSeller(Seller s)
+        {
+            Sellers.Add(s);
+        }
+        public double totalSales(DateTime initial , DateTime Final)
+        {
+            return Sellers.Sum(seller => seller.totalSales(initial,Final));
+        }
+    }
+}
